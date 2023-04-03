9fbf92f5afc95905322751aa0ca731f4f381e4af
Paulina <pjlobocka@gmail.com>
2023-04-04 00:49:11 +0200

    instrukcja bazowa

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="small-container">
+    <h1>Znajomi</h1>
+    
+    <person-form @add:person="addPerson"/>
+    <persons-table :personsSource="persons"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import PersonsTable from '@/components/PersonsTable.vue'
+  import PersonForm from '@/components/PersonForm.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    components: {
+      PersonsTable,
+      PersonForm,
+    },
+    data() {
+      return {
+        persons: []
+      }
+    },
+    methods: {
+      addPerson(person) {
+        this.persons = [...this.persons, person] // tu nowo dodany pracownik nie otrzymuje unikalnego id
+      },
+      async getPersons() {
+        try {
+          const response = await fetch('https://jsonplaceholder.typicode.com/users')
+          const data = await response.json()
+          this.persons = data
+        } catch (error) {
+          console.error(error)
+        }
+      },
+    },
+    mounted() {
+      this.getPersons()
+    },
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+   button {
+    background: #009435;
+    border: 1px solid #009435;
+  }
+  .small-container {
+    max-width: 680px; 
+  }
 </style>
